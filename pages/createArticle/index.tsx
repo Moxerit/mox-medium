bce544fdd709b73d81137ce3f64b6cdac341849e
Chase Alber <calber2017@gmail.com>
2023-01-31 09:58:03 -0700

    article page done and started mainfeed page

--- pages/createArticle/index.tsx
+++ pages/createArticle/index.tsx
@@ -7,11 +7,44 @@ import {
   User,
 } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
+import { useState } from "react";
 
 const CreateArticle: NextPage = () => {
   const supabaseClient = useSupabaseClient();
   const user = useUser();
   const router = useRouter();
+
+  const initialState = {
+    title: "",
+    content: "",
+  };
+
+  const [articleData, setArticleData] = useState(initialState);
+
+  const handleChange = (e: any) => {
+    setArticleData({ ...articleData, [e.target.name]: e.target.value });
+  };
+
+  const createArticle = async () => {
+    try {
+      const { data, error } = await supabaseClient
+        .from("articles")
+        .insert([
+          {
+            title: articleData.title,
+            content: articleData.content,
+            user_email: user?.email?.toLowerCase(),
+            user_id: user?.id,
+          },
+        ])
+        .single();
+      if (error) throw error;
+      setArticleData(initialState);
+      router.push("/mainFeed");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
 
   return (
     <>
@@ -22,11 +55,28 @@ const CreateArticle: NextPage = () => {
             name="title"
             aria-label="title"
             placeholder="Article Title"
-            fullWidth={True}
+            fullWidth={true}
             rows={1}
             size="xl"
+            onChange={handleChange}
           />
         </Grid>
+        <Text h3>Article Text</Text>
+        <Grid xs={12}>
+          <Textarea
+            name="content"
+            aria-label="content"
+            placeholder="Article Content"
+            fullWidth={true}
+            rows={6}
+            size="xl"
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid xs={12}>
+          <Text>Posting as {user?.email}</Text>
+        </Grid>
+        <Button onPress={createArticle}>Create Article</Button>
       </Grid.Container>
     </>
   );
